c54d264a4eeeffc39be9ed637f73c6142f8534a0
ghashd <ghashd@ghashd-ubuntu-dell>
2013-09-18 14:53:54 +0200

    classes for queryMonitor and StoreMock - CPU version of store

---
+++ src/CPU_MOCKS/StoreMock.cpp
@@ -0,0 +1,23 @@
+/*
+ * StoreMock.cpp
+ *
+ *  Created on: 17-09-2013
+ *      Author: ghashd
+ */
+
+#include "StoreMock.h"
+
+namespace ddj {
+namespace store {
+
+StoreMock::StoreMock() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StoreMock::~StoreMock() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace store */
+} /* namespace ddj */
