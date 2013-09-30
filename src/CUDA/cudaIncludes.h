021672e3db3089b3ae12be660036f1efb23d5d78
kdutkowski <kdutkowski@gmail.com>
2013-09-30 19:50:01 +0200

    added missing header file

---
+++ src/CUDA/cudaIncludes.h
@@ -0,0 +1,12 @@
+#ifndef CUDAINCLUDES_H_
+#define CUDAINCLUDES_H_
+
+#define CUDA_CHECK_RETURN(value) {											\
+	cudaError_t _m_cudaStat = value;										\
+	if (_m_cudaStat != cudaSuccess) {										\
+		fprintf(stderr, "Error %s at line %d in file %s\n",					\
+				cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);		\
+		exit(1);															\
+	} }
+
+#endif
