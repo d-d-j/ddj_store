654d0dc8c0167e61ba6bc622c8b70cac99529b19
Karol Dzitkowski <k.dzitkowski@gmail.com>
2013-11-19 17:46:56 +0100

    + add storeSettings back

---
+++ src/Store/storeSettings.h
@@ -0,0 +1,14 @@
+/*
+ * storeSettings.h
+ *
+ *  Created on: 19-11-2013
+ *      Author: ghash
+ */
+
+#ifndef STORESETTINGS_H_
+#define STORESETTINGS_H_
+
+#define DEVICE_BUFFERS_COUNT 10
+#define STORE_BUFFER_SIZE 	 128
+
+#endif /* STORESETTINGS_H_ */
