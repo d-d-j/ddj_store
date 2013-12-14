5f72f97d3545311d5b71cda8dc1243e3abbb026a
Karol Dzitkowski <k.dzitkowski@gmail.com>
2013-12-14 01:05:04 +0100

    + gtest install script

--- gtest_install.sh
+++ gtest_install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+mkdir gtestInstallTemp
+cd gtestInstallTemp
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo apt-get install libgtest-dev
+    wget https://googletest.googlecode.com/files/gtest-1.7.0.zip
+    unzip gtest-1.7.0.zip
+    cd gtest-1.7.0
+	mkdir mybuild
+	cd mybuild
+	cmake -DGTEST_HAS_PTHREAD=1 ../
+	make
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	mkdir gtestInstallTemp
-	cd gtestInstallTemp
 	curl -O https://googletest.googlecode.com/files/gtest-1.7.0.zip
 	unzip gtest-1.7.0.zip
 	cd gtest-1.7.0
 	mkdir mybuild
 	cd mybuild
-	cmake -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++ -U__STRICT_ANSI__" -DGTEST_HAS_PTHREAD=1 ../     
-	cp -r ../include/gtest /usr/local/include
-	cp lib*.a /usr/local/lib
-	cd ../../../
-	rm -R gtestInstallTemp
+	cmake -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++ -U__STRICT_ANSI__" -DGTEST_HAS_PTHREAD=1 ../
+	make
 fi
+sudo cp -r ../include/gtest /usr/local/include
+sudo cp lib*.a /usr/local/lib
+cd ../../../
+rm -R gtestInstallTemp
