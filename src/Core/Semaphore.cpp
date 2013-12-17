470492d97a80cb6e395cf01f6472bde8c4b74be8
Karol Dzitkowski <k.dzitkowski@gmail.com>
2013-12-17 01:55:13 +0100

    + try to fix semaphore

--- src/Core/Semaphore.cpp
+++ src/Core/Semaphore.cpp
@@ -20,16 +20,21 @@ namespace ddj {
 	void Semaphore::Wait()
 	{
 		boost::mutex::scoped_lock lock(_mutex);
-		if(_value >= _max)
+		for(;_value >= _max;)
+		{
 			_cond.wait(lock);
+		}
 		_value++;
 	}
 
 	void Semaphore::Release()
 	{
 		boost::mutex::scoped_lock lock(_mutex);
-		_value--;
-		_cond.notify_one();
+		if(_value > 0)
+		{
+			_value--;
+			_cond.notify_one();
+		}
 	}
 
 } /* namespace ddj */
