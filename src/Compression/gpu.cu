bb3c4ec0443178f4ffa39018f62e3c244c353494
kdutkowski <kdutkowski@gmail.com>
2014-01-05 12:24:28 +0100

    renamed parameter in compression methods

--- src/Compression/gpu.cu
+++ src/Compression/gpu.cu
@@ -4,11 +4,11 @@
 // celowe rozwinięcie pętli
 // id - id bloku do dekompresji
 // off o ile przesunac dane
-#define FETCH_DATA1(level, data, id, off, dev_out) __fetch(level, data, id, 0, off, dev_out);
-#define FETCH_DATA2(level, data, id, off, dev_out) __fetch(level, data, id, 0, off, dev_out); __fetch(level, data, id, 1, off, dev_out);
-#define FETCH_DATA3(level, data, id, off, dev_out) __fetch(level, data, id, 0, off, dev_out); __fetch(level, data, id, 1, off, dev_out); __fetch(level, data, id, 2, off , dev_out);
-#define FETCH_DATA4(level, data, id, off, dev_out) __fetch(level, data, id, 0, off, dev_out); __fetch(level, data, id, 1, off, dev_out); __fetch(level, data, id, 2, off , dev_out); __fetch(level, data, id, 3, off, dev_out);
-#define FETCH_DATA5(level, data, id, off, dev_out) __fetch(level, data, id, 0, off, dev_out); __fetch(level, data, id, 1, off, dev_out); __fetch(level, data, id, 2, off , dev_out); __fetch(level, data, id, 3, off, dev_out); __fetch(level, data, id, 4, off, dev_out);
+#define FETCH_DATA1(level, data, id, off, compressed_data) __fetch(level, data, id, 0, off, compressed_data);
+#define FETCH_DATA2(level, data, id, off, compressed_data) __fetch(level, data, id, 0, off, compressed_data); __fetch(level, data, id, 1, off, compressed_data);
+#define FETCH_DATA3(level, data, id, off, compressed_data) __fetch(level, data, id, 0, off, compressed_data); __fetch(level, data, id, 1, off, compressed_data); __fetch(level, data, id, 2, off , compressed_data);
+#define FETCH_DATA4(level, data, id, off, compressed_data) __fetch(level, data, id, 0, off, compressed_data); __fetch(level, data, id, 1, off, compressed_data); __fetch(level, data, id, 2, off , compressed_data); __fetch(level, data, id, 3, off, compressed_data);
+#define FETCH_DATA5(level, data, id, off, compressed_data) __fetch(level, data, id, 0, off, compressed_data); __fetch(level, data, id, 1, off, compressed_data); __fetch(level, data, id, 2, off , compressed_data); __fetch(level, data, id, 3, off, compressed_data); __fetch(level, data, id, 4, off, compressed_data);
 
 #define FIX_BLOCK(level, out, in, id) char *out = (char *) in; out += (id * level) % 4  // przesuniecie w bloku pamieci
 
@@ -47,21 +47,21 @@ __global__ void compress_var (int bl, int *in, char *out)
         out[id*bl+i] = b_out[i];
 }
 
-__global__ void decompress_var (int bl, int *out, char *dev_out)
+__global__ void decompress_var (int bl, int *out, char *compressed_data)
 {
     long id = threadIdx.x + blockIdx.x * blockDim.x; // id bloku do dekompresji
     int tmp1[8], tmp2[8]; // tymczasowe tablice do dekompresji
 
     // dekompresja bloku id, uzywa do dekompresji tablic, wyniki skladuje w data + id * 8
-    pfor_func[bl-2](id, 0, tmp1, tmp2, tmp2, dev_out);
+    pfor_func[bl-2](id, 0, tmp1, tmp2, tmp2, compressed_data);
 
     for (int i = 0; i < 8; i++) {
         out[id*8+i] = tmp2[i];
     }
 }
 
-inline __device__ void __fetch(int level, int *data, int id, int pos, int off, char* dev_out) {
-    data[pos] = *((int*)dev_out + off + (id * level) / 4 + pos);
+inline __device__ void __fetch(int level, int *data, int id, int pos, int off, char* compressed_data) {
+    data[pos] = *((int*)compressed_data + off + (id * level) / 4 + pos);
 }
 
 //Pobiera dane i dekompresuje n-ty skompresowany blok
@@ -77,9 +77,9 @@ __device__ void copyDecompressed(int *in, int *out)
     out[7]=in[7];
 }
 
-__device__ void gpu_decompress2(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress2(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA1(2, r_in, id, offset, dev_out);
+    FETCH_DATA1(2, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(2, tmp_in, r_in, id);
 
@@ -87,126 +87,126 @@ __device__ void gpu_decompress2(int id, int offset, int *r_in, int *r_out, int *
 }
 
 
-__device__ void gpu_decompress3(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress3(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA2(3, r_in, id, offset, dev_out);
+    FETCH_DATA2(3, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(3, tmp_in, r_in, id);
 
     DECOMP(3, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress4(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress4(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA1(4, r_in, id, offset, dev_out);
+    FETCH_DATA1(4, r_in, id, offset, compressed_data);
 
     char *tmp_in = (char *) r_in;
 
     DECOMP(4, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress5(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress5(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA2(5, r_in, id, offset, dev_out);
+    FETCH_DATA2(5, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(5, tmp_in, r_in, id);
 
     DECOMP(5, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress6(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress6(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA2(6, r_in, id, offset, dev_out);
+    FETCH_DATA2(6, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(6, tmp_in, r_in, id);
 
     DECOMP(6, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress7(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress7(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA3(7, r_in, id, offset, dev_out);
+    FETCH_DATA3(7, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(7, tmp_in, r_in, id);
 
     DECOMP(7, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress8(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress8(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA2(8, r_in, id, offset, dev_out);
+    FETCH_DATA2(8, r_in, id, offset, compressed_data);
 
     char * tmp_in = (char *) r_in;
 
     DECOMP(8, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress9(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress9(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA3(9, r_in, id, offset, dev_out);
+    FETCH_DATA3(9, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(9, tmp_in, r_in, id);
 
     DECOMP(9, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress10(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress10(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA3(10, r_in, id, offset, dev_out);
+    FETCH_DATA3(10, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(10, tmp_in, r_in, id);
 
     DECOMP(10, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress11(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress11(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA4(11, r_in, id, offset, dev_out);
+    FETCH_DATA4(11, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(11, tmp_in, r_in, id);
 
     DECOMP(11, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress12(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress12(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA3(12, r_in, id, offset, dev_out);
+    FETCH_DATA3(12, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(12, tmp_in, r_in, id);
 
     DECOMP(12, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress13(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress13(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA4(13, r_in, id, offset, dev_out);
+    FETCH_DATA4(13, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(13, tmp_in, r_in, id);
 
     DECOMP(13, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress14(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress14(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA4(14, r_in, id, offset, dev_out);
+    FETCH_DATA4(14, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(14, tmp_in, r_in, id);
 
     DECOMP(14, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress15(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress15(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA5(15, r_in, id, offset, dev_out);
+    FETCH_DATA5(15, r_in, id, offset, compressed_data);
 
     FIX_BLOCK(15, tmp_in, r_in, id);
 
     DECOMP(15, id, tmp_in, r_out, out);
 }
 
-__device__ void gpu_decompress16(int id, int offset, int *r_in, int *r_out, int *out, char* dev_out)
+__device__ void gpu_decompress16(int id, int offset, int *r_in, int *r_out, int *out, char* compressed_data)
 {
-    FETCH_DATA4(16, r_in, id, offset, dev_out);
+    FETCH_DATA4(16, r_in, id, offset, compressed_data);
 
     char * tmp_in = (char *) r_in;
 
