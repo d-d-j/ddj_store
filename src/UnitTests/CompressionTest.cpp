7e550705884bd5edc74c36064ab2f78f78cba180
kdutkowski <kdutkowski@gmail.com>
2014-01-05 12:43:05 +0100

    fixed compression test

--- src/UnitTests/CompressionTest.cpp
+++ src/UnitTests/CompressionTest.cpp
@@ -14,7 +14,7 @@ TEST_F(CompressionTest, Compress_And_Decompress_Data)
 
 	int max_size = 800000;
 	char *dev_out;
-	int *dev_data;
+	int *dev_data,*dev_data2;
 
 	int *host_data, *host_data2;
 
@@ -24,19 +24,20 @@ TEST_F(CompressionTest, Compress_And_Decompress_Data)
 	big_random_block(max_size, 10, host_data);
 
 	cudaMalloc((void **) &dev_out, max_size);
-
+	cudaMalloc((void **) &dev_data2, max_size * sizeof(int));
 	cudaMalloc((void **) &dev_data, max_size * sizeof(int));
 	cudaMemcpy(dev_data, host_data, max_size * sizeof(int),
 			cudaMemcpyHostToDevice);
 
 	compressVar(max_size, 5, dev_data, dev_out);
 
-	decompressVar(max_size, 5, dev_data, dev_out);
+	decompressVar(max_size, 5, dev_data2, dev_out);
 
-	cudaMemcpy(host_data2, dev_data, max_size * sizeof(int),
+	cudaMemcpy(host_data2, dev_data2, max_size * sizeof(int),
 			cudaMemcpyDeviceToHost);
 	cudaFree(dev_out);
 	cudaFree(dev_data);
+	cudaFree(dev_data2);
 
 	for (int i = 0; i < max_size; i++)
 	{
