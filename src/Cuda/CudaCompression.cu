10d0449bebec4af028ef1251a981cece8e1fa8ab
kdutkowski <kdutkowski@gmail.com>
2014-01-07 16:35:11 +0100

    fixed compression test

--- src/Cuda/CudaCompression.cu
+++ src/Cuda/CudaCompression.cu
@@ -2,10 +2,10 @@
 
 void compressVar(int max_size, int bl, int *dev_data, char* dev_out)
 {
-	compress_var <<<max_size / (8*1000) , 100>>> (bl, dev_data, dev_out);
+	compress_var <<<max_size / (8*100) , 100>>> (bl, dev_data, dev_out);
 }
 
 void decompressVar(int max_size, int bl, int *dev_data, char* dev_out)
 {
-	decompress_var<<<max_size / (8*1000), 100 >>> (bl, dev_data, dev_out);
+	decompress_var<<<max_size / (8*100), 100 >>> (bl, dev_data, dev_out);
 }
