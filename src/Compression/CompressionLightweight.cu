c41992390d65270310f536c9b9c2c63a401c5626
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-01-15 21:25:19 +0100

    + fit compression to trunk size

--- src/Compression/CompressionLightweight.cu
+++ src/Compression/CompressionLightweight.cu
@@ -16,7 +16,7 @@ void copyBytes(unsigned char * dest, const unsigned char * source,
 
 __global__
 void EncodeKernel(storeElement * in_d, unsigned char * out_d) {
-	int index = blockIdx.x * THREADS + threadIdx.x;
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
 	int32_t low = in_d[index].time & 0xFFFFFFFF;
 	int32_t high = (in_d[index].time >> 32) & 0xFFFFFFFF;
 	int32_t position = 10 * index + 4;
@@ -41,7 +41,7 @@ void EncodeKernel(storeElement * in_d, unsigned char * out_d) {
 
 __global__
 void DecodeKernel(unsigned char * in_d, storeElement * out_d) {
-	int index = blockIdx.x * THREADS + threadIdx.x;
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
 	converter c;
 	copyBytes(c.fromBytes, in_d, 4);
 
@@ -64,32 +64,38 @@ void DecodeKernel(unsigned char * in_d, storeElement * out_d) {
 
 size_t CompressLightweight(storeElement* elements, size_t size, unsigned char** result)
 {
+	int elemCount = size / sizeof(storeElement);
+
 	//prepare compression output
 	unsigned char *compressionOutput_device;	//output space for compressed data
-	cudaMalloc((void**) &compressionOutput_device, COMPRESSED_DATA_SIZE);
-	cudaMemset(compressionOutput_device, 0, COMPRESSED_DATA_SIZE);
+	cudaMalloc((void**) &compressionOutput_device, COMPRESSED_DATA_SIZE(elemCount));
+	cudaMemset(compressionOutput_device, 0, COMPRESSED_DATA_SIZE(elemCount));
 
 	//compress
-	EncodeKernel<<<BLOCKS, THREADS>>>(elements, compressionOutput_device);
+	int blocks = (elemCount + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+	EncodeKernel<<<blocks, THREADS_PER_BLOCK>>>(elements, compressionOutput_device);
 	cudaDeviceSynchronize();
 
 	//return result
 	(*result) = compressionOutput_device;
-	return COMPRESSED_DATA_SIZE;
+	return COMPRESSED_DATA_SIZE(elemCount);
 }
 
 size_t DecompressLightweight(unsigned char* data, size_t size, storeElement** result)
 {
+	int elemCount = size / COMPRESSED_ELEMENT_SIZE;
+
 	// prepare decompression output
 	storeElement* decompressionOutput_device;	//output space for decompressed data
-	cudaMalloc((void**) &decompressionOutput_device, DATA_SIZE);
-	cudaMemset(decompressionOutput_device, 0, DATA_SIZE);
+	cudaMalloc((void**) &decompressionOutput_device, elemCount * sizeof(storeElement));
+	cudaMemset(decompressionOutput_device, 0, elemCount * sizeof(storeElement));
 
 	//decompress
+	int blocks = (elemCount + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
 	unsigned char *decompessionInput_device = data;
-	DecodeKernel<<<BLOCKS, THREADS>>>(decompessionInput_device, decompressionOutput_device);
+	DecodeKernel<<<blocks, THREADS_PER_BLOCK>>>(decompessionInput_device, decompressionOutput_device);
 	cudaDeviceSynchronize();
 
 	(*result) = decompressionOutput_device;
-	return DATA_SIZE;
+	return elemCount * sizeof(storeElement);
 }
