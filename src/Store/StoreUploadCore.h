29736db58b1df3e1f79748131fbaba5632a062c6
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-01-26 11:07:00 +0100

    + StoreUpload.h documented

--- src/Store/StoreUploadCore.h
+++ src/Store/StoreUploadCore.h
@@ -1,10 +1,3 @@
-/*
- * GpuUploaderCore.h
- *
- *  Created on: 22-09-2013
- *      Author: ghashd
- */
-
 #ifndef GPUUPLOADCORE_H_
 #define GPUUPLOADCORE_H_
 
@@ -16,7 +9,12 @@
 
 namespace ddj {
 namespace store {
-
+	/**
+	 *  A class used to save an array of elements to Database.
+	 *  A class with one public function Upload which uploads an array of StoreElements to main database memory on GPU.
+	 *  It also sorts an array before upload and compress it using Compress class object. It produces a structure
+	 *  storeTrunkInfo with information about location of uploaded array in GPU memory.
+	 */
 	class StoreUploadCore
 	{
 		CudaController* _cudaController;
@@ -28,8 +26,24 @@ namespace store {
 		Config* _config;
 
 	public:
+		/**
+		* A constructor with CudaController pointer as only parameter.
+		* It initializes logger and config and sets a pointer to cuda controller.
+		* @param cudaController a pointer to CudaController used to access
+		* memory on GPU side used by database, and for getting CUDA streams.
+		*/
 		StoreUploadCore(CudaController* cudaController);
+
+		/**
+		 * A virtual empty destructor.
+		 */
 		virtual ~StoreUploadCore();
+
+		/**
+		 * The only public method used to upload an array of elements (time series records) to main DB's memory on GPU.
+		 * @param elements an array of storeElements (time series records stored in DB).
+		 * @param elementsToUploadCount a number of elements to upload to GPU.
+		 */
 		storeTrunkInfo* Upload(storeElement* elements, int elementsToUploadCount);
 
 	private:
